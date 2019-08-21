75ff7aaf9b50be8ac6c945467559ea4c86f62da7
Bogdan Guna <bogdan.guna@gmail.com>
2019-08-21 14:30:13 +0100

    added slice exercise

--- cmd/slices/main.go
+++ cmd/slices/main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello slices")
+	parents := []string{"Carol", "Mike"}
+	kids := []string{"Marcia", "Jan", "Cindy", "Greg", "Peter", "Bobby"}
+
+	family := append(parents, kids...)
+	fmt.Printf("Family: %v\n", family)
+
+	fmt.Printf("Len of family: %v\n", len(family))
+	fmt.Printf("Cap of family: %v\n", cap(family))
+
+	youngest := family[len(family)-3:]
+	fmt.Printf("Youngest: %v\n", youngest)
+
+	extras := make([]string, 1, 1)
+	extras[0] = "Alice"
+	fmt.Println(extras)
 }
